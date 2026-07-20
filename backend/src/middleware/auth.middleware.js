1f3d20dfb9cc55b4098d9ba78cf6c6dd9b64ee50
Hüseyin Emeci <huseyinemeci@gmail.com>
2026-07-20 23:52:21 +0300

    User import fix in auth.middleware.js

--- backend/src/middleware/auth.middleware.js
+++ backend/src/middleware/auth.middleware.js
@@ -1,5 +1,5 @@
 import { getAuth } from "@clerk/express";
-import { User } from "../models/user.model.js";
+import User from "../models/user.model.js";
 
 export async function protectRoute(req, res, next) {
     try {
